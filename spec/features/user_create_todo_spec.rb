a0d81487e8d5acca0a4afa18146069b277ff3875
nat14 <naryucha14@gmail.com>
2016-08-10 19:30:58 -0700

    added user create todo feature spec

---
+++ spec/features/user_create_todo_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'user creates todo' do
+  scenario 'successfully' do
+    visit root_path
+    click_on 'Add a new todo'
+    fill_in 'Title', with: 'Read a Book'
+    click_on 'Submit'
+
+    expect(page).to have_css '.todos li', text: 'Read a Book'
+  end
+end
